012b31969dafb6dfcb5e60ab2477db2db0ebeb21
huang.zhangjiang <huang.zhangjiang@mwee.cn>
2020-05-12 11:43:33 +0800

    update

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -3,15 +3,18 @@ import * as T from '../actionTypes';
 import * as todoActions from '../actions/todo';
 import { API } from '../services/todo';
 
+// 处理
 export function* initTodoAsync() {
     const todolist = yield call(API.initTodo);
     yield put(todoActions.initTodo(todolist));
 }
 
+// 监听
 export function* watchInitTodoAsync() {
     yield takeEvery(T.FETCH_TODO, initTodoAsync);
 }
 
+// 执行监听
 export default function* rootSaga() {
     yield all([fork(watchInitTodoAsync)]);
 }
